090f7be43bfe6729b31a913759f3b660c80600cd
MBStarup <mavi.nexu@gmail.com>
2023-01-26 00:56:01 +0100

    Parse func v0 done. Future: Refactor out peek(), as it forces clone and is cringe to work with. Maybe next() as default and then the  few cases where we want to keep a terminating token (for parens/curlies/semicolons) use a prepend() or something or make a slice or a hard cap hard coded as different params or drop teh iter and just make it an index in an array. Consolidate things that evaluate to a value (Free standing symbols, function calls and numbers), as they all need the same check for operators after.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use core::panic;
+use std::iter::Peekable;
+
 struct CharProvider {
     position: usize,
     data: String,
@@ -34,7 +37,7 @@ where
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Token {
     WhiteSpace,
     Operator(String),
@@ -71,6 +74,7 @@ where
             },
 
             Option::Some(c) if c.is_ascii_digit() => {
+                //* Int declaration
                 let mut num_str = c.to_string();
                 while let Option::Some(c) = self.char_provider.peek() {
                     if c.is_ascii_digit() {
@@ -82,10 +86,10 @@ where
                 }
                 Option::Some(Token::Number(num_str.parse().expect(format!("Could not parse number {}", num_str).as_str())))
             },
-            Option::Some(c) if c.is_ascii_alphabetic() || c == '_' => {
+            Option::Some(c) if c.is_ascii_alphabetic() || c == '_' || c == '!' => {
                 let mut symbol_str = c.to_string();
                 while let Option::Some(c) = self.char_provider.peek() {
-                    if c.is_ascii_alphabetic() || c == &'_' {
+                    if c.is_ascii_alphabetic() || c == &'_' || c == &'!' {
                         symbol_str += &c.to_string();
                         self.char_provider.next();
                     } else {
@@ -109,10 +113,10 @@ where
                 self.next()
             },
             Option::Some(c) if c == ':' => Some(Token::Colon),
-            Option::Some(c) if c == ';' => Some(Token::SemiColon),
+            Option::Some(c) if c == ';' => Some(Token::SemiColon), //* Function declaration
             Option::Some(c) if c == '(' => Some(Token::ParensStart),
             Option::Some(c) if c == ')' => Some(Token::ParensEnd),
-            Option::Some(c) if "+-*/&|!=<>".contains(c) => Some(Token::Operator(c.to_string())),
+            Option::Some(c) if "+-*/&|=<>".contains(c) => Some(Token::Operator(c.to_string())),
             Option::None => Option::None,
 
             Option::Some(c) => panic!("Error parsing: {}", c),
@@ -169,10 +173,151 @@ fn main() {
                   newline()
               }
               "
-        .to_string(),
+        .to_owned(),
     };
 
-    //text.data = "test this 77 ;; ()".to_owned();
+    text.data = "
+    {
+        # My giga scuffed standard library LULE #
+        
+        _:;x;{x} # identity function since we can't use parenthesis in expressions #
+        
+        
+        printDigit: ;num; print(48+num)
+        newline: ;; print(10)
+        if: ;bool action; { res:0 bool & res:action() res } # if \"statement\" using short circuit evaluation. Returns 0 if it didn't run, otherwise return the result of the action #
+        ifElse: ;bool ifAction elseAction; { res:elseAction bool & { res:ifAction 1 } res() }
+        !: ;bool; bool = 0
+        loopWithBase: ;predicate action base; { # Takes a predicate acting on the loop count, and an action acting on the loop count to perform while the predicate evaluates true #
+            recusiveHelper: ;count prev; {
+                ifElse(predicate(count + 1) ;;recusiveHelper(count + 1 action(count prev)) ;;action(count prev))
+            }
+            ifElse(predicate(0) ;;recusiveHelper(0 base) ;;base )
+        }
+        pow: ;base exponent; loopWithBase(;i; {i < exponent + 1} ;i res; {res * base} 1)/base
+        printNum: ;x; { # xd no shot I figure out how to do this with the loop func, garbage lang #
+            recusiveHelper: ;i prev; {
+                if(_(prev/10) > 0 ;;recusiveHelper(i + 1 prev/10))
+                printDigit(prev - _( _(prev/10) * 10))
+            }
+            if(x > 0 ;;recusiveHelper(0 x))
+        }
+        # stdlib end #
+        newline()
+        printNum(pow(3 10))
+        newline()
+    }"
+    .to_owned();
+
     let mut lexer = TokenProvider::new(&mut text);
-    lexer.for_each(|t| println!("{:?}", t))
+
+    let mut ast = parse(lexer);
+    println!("{:?}", ast);
+}
+
+fn parse<'a, T: Iterator<Item = Token>>(tokens: T) -> Option<Expr> {
+    fn parse_rec<T: Iterator<Item = Token>>(tokens: &mut Peekable<T>) -> Option<Expr> {
+        match tokens.next() {
+            Some(Token::CurlyStart) => {
+                let mut res: Vec<Expr> = Vec::new();
+
+                while match tokens.peek() {
+                    Some(Token::CurlyEnd) => false,
+                    Some(_) => true,
+                    None => panic!("Unclosed multiExpr"),
+                } {
+                    res.push(parse_rec(tokens).unwrap())
+                }
+
+                tokens.next(); // Remove curly end
+
+                Some(Expr::MultiExpr(res))
+            },
+            //Cringe clone occurs
+            Some(Token::Symbol(symbol)) => match tokens.peek().cloned() {
+                Some(Token::Colon) => {
+                    tokens.next();
+                    Some(Expr::Assign(Box::new(parse_rec(tokens).unwrap_or_else(|| panic!("Cannot assign empty value to {symbol:?}"))), symbol))
+                },
+                Some(Token::Operator(operator)) => {
+                    tokens.next();
+                    Some(Expr::BiExpr(
+                        Box::new(Expr::Eval(symbol)),
+                        Box::new(parse_rec(tokens).unwrap_or_else(|| panic!("Missing right side of BiExpr {operator:?}"))),
+                        operator,
+                    ))
+                },
+                Some(Token::ParensStart) => {
+                    let mut params: Vec<Expr> = Vec::new();
+                    tokens.next();
+
+                    while match tokens.peek() {
+                        Some(Token::ParensEnd) => false,
+                        Some(_) => true,
+                        None => panic!("Unclosed function call"),
+                    } {
+                        params.push(parse_rec(tokens).unwrap())
+                    }
+
+                    tokens.next(); // Remove ParensEnd
+
+                    match tokens.peek().cloned() {
+                        Some(Token::Operator(operator)) => {
+                            tokens.next();
+                            Some(Expr::BiExpr(
+                                Box::new(Expr::Call(params, symbol)),
+                                Box::new(parse_rec(tokens).unwrap_or_else(|| panic!("Missing right side of BiExpr {operator:?}"))),
+                                operator,
+                            ))
+                        },
+                        _ => Some(Expr::Call(params, symbol)),
+                    }
+                },
+                Some(_) => Some(Expr::Eval(symbol)),
+                None => None,
+            },
+            //Cringe clone occurs
+            Some(Token::Number(number)) => match tokens.peek().cloned() {
+                Some(Token::Operator(operator)) => {
+                    tokens.next();
+                    Some(Expr::BiExpr(
+                        Box::new(Expr::Int(number)),
+                        Box::new(parse_rec(tokens).unwrap_or_else(|| panic!("Missing right side of BiExpr {operator:?}"))),
+                        operator,
+                    ))
+                },
+                _ => Some(Expr::Int(number)),
+            },
+            Some(Token::SemiColon) => {
+                let mut params: Vec<String> = Vec::new();
+
+                while match tokens.next() {
+                    Some(Token::SemiColon) => false,
+                    Some(Token::Symbol(symbol)) => {
+                        params.push(symbol.to_owned()); // idk about this one homie...
+                        true
+                    },
+                    Some(token) => panic!("Unknown token ({token:?}) in function params"),
+                    None => panic!("Unclosed function declaration"),
+                } {}
+
+                Some(Expr::Func(Box::new(parse_rec(tokens).expect("Function body missing")), params))
+            },
+            None => None,
+            Some(token) => panic!("Bad token: {token:?}"),
+        }
+    }
+
+    parse_rec(&mut tokens.peekable())
+}
+
+#[derive(Debug)]
+enum Expr {
+    MultiExpr(Vec<Expr>),
+    Int(i32),
+    Eval(String),
+    BiExpr(Box<Expr>, Box<Expr>, String),
+    Func(Box<Expr>, Vec<String>),
+    Assign(Box<Expr>, String),
+    Call(Vec<Expr>, String),
 }
